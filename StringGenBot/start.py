7cf8e651880649f744966934d978db0e31ab97a0
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-30 05:58:40 +0530

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,22 +12,22 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""𝐇𝐞𝐲 {msg.from_user.mention}🍷,
+        text=f"""<b>𝐇𝐞𝐲 {msg.from_user.mention}🍷,
 
 ɪ ᴀᴍ {me2},
 ᴛʀᴜsᴛᴇᴅ 𝗦𝗧𝗥𝗜𝗡𝗚 𝗚𝗥𝗡𝗘𝗥𝗔𝗧𝗢𝗥 ʙᴏᴛ.
 ғᴜʟʟʏ sᴀғᴇ & sᴇᴄᴜʀᴇ.
 ɴᴏ ᴀɴʏ ᴇʀʀᴏʀ
 
-𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝗡𝗲𝗶𝗺𝗮𝗻](tg://user?id={OWNER_ID}) !""",
+Made With By : [VJ Botz](https://t.me/VJ_Botz) !</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="⚡𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆⚡", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("❣️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ❣️", url="https://t.me/Neiman_X_Support"),
-                    InlineKeyboardButton("🥀 𝐎𝐟𝐟𝐢𝐜𝐞 🥀", url="https://t.me/DETECTED_09")
+                    InlineKeyboardButton("❣️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ❣️", url="https://t.me/VJ_Bot_Disscussion"),
+                    InlineKeyboardButton("🥀 𝐎𝐟𝐟𝐢𝐜𝐞 🥀", url="https://t.me/VJ_Botz")
                 ]
             ]
         ),
