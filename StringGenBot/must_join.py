f5de71fadd1ac0a4a699344d216f2384854c7817
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-30 06:05:58 +0530

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -20,11 +20,11 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://te.legra.ph/file/a35aeb698a07c50f8dfc3.jpg", caption=f"» 𝐅𝐈𝐑𝐒𝐓𝐋𝐘 𝐘𝐎𝐔 𝐍𝐄𝐄𝐃 𝐓𝐎 𝐉𝐎𝐈𝐍 𝐎𝐔𝐑 𝐅𝐀𝐌𝐈𝐋𝐘 𝐓𝐇𝐄𝐍 𝐘𝐎𝐔 𝐂𝐀𝐍 𝐔𝐒𝐄 𝐌𝐄 [𝐎𝐅𝐅𝐈𝐂𝐄]({link}). 𝐀𝐅𝐓𝐄𝐑 𝐉𝐎𝐈𝐍 𝐒𝐓𝐀𝐑𝐓 𝐌𝐄 𝐀𝐆𝐀𝐈𝐍 !",
+                    photo="https://telegra.ph/file/5c586e00f34665267ab5b.jpg", caption=f"» 𝐅𝐈𝐑𝐒𝐓𝐋𝐘 𝐘𝐎𝐔 𝐍𝐄𝐄𝐃 𝐓𝐎 𝐉𝐎𝐈𝐍 𝐎𝐔𝐑 𝐅𝐀𝐌𝐈𝐋𝐘 𝐓𝐇𝐄𝐍 𝐘𝐎𝐔 𝐂𝐀𝐍 𝐔𝐒𝐄 𝐌𝐄 <b>[Update Channel]({link}).</b> 𝐀𝐅𝐓𝐄𝐑 𝐉𝐎𝐈𝐍 𝐒𝐓𝐀𝐑𝐓 𝐌𝐄 𝐀𝐆𝐀𝐈𝐍 !",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> 𝐎𝐅𝐅𝐈𝐂𝐄", url=link),
+                                InlineKeyboardButton("</> Update Channel", url=link),
                             ]
                         ]
                     )
