5b3f6cb67c7d983d52176e97a17ec8994acc5a57
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-11-04 10:24:44 +0530

    Update clone.py

--- StringGenBot/clone.py
+++ StringGenBot/clone.py
@@ -226,7 +226,7 @@ async def cloned_count(client, message):
         bot_usernames_text = '\n'.join(bot_usernames)
         await message.reply_text(f"{count} bots have been cloned:\n\n{bot_usernames_text}")
 
-@Client.on_message(filters.command(["removebot"]) & filters.user(ADMINS))
+@Client.on_message(filters.command(["removebot"]) & filters.user(OWNER_ID))
 async def remove_bot(client: Client, message: Message):
     try:
         bot_username = message.text.split(" ", maxsplit=1)[1].strip()
