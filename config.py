dc2a7b27d354d2daaa5152a7872501c3d8cfe2bb
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-30 08:06:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
+OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
+
+# Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
+F_SUB = int(environ.get("F_SUB", ""))
 
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
