22b5e72b46acce6f2fc92a806520d6871a4c7e13
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-01-19 15:57:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 from os import environ
 
+# Telegram Account Api Id And Api Hash
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 
+# Your Main Bot Token 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
+# Owner ID For Broadcasting 
 OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
 F_SUB = environ.get("F_SUB", "")
 
+# Mongodb Database Uri For User Data Store 
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
+
+# Port To Run Web Application 
+PORT = int(environ.get('PORT', 8080))
