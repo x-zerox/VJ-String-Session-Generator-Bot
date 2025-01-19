141a59cac239d48563db700a2bd3fac28414fc7a
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-01-19 16:11:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-from flask import Flask
-from config import API_ID, API_HASH, BOT_TOKEN, PORT
-from pyrogram import Client, idle
+from config import API_ID, API_HASH, BOT_TOKEN
 
-app = Flask(__name__)
-
-# Initialize the Pyrogram Client
 bot = Client(
     "Anonymous",
     api_id=API_ID,
@@ -14,24 +9,7 @@ bot = Client(
     plugins=dict(root="TechVJ"),
 )
 
-@app.route('/')
-def health_check():
-    return "Bot is running!", 200
-
 if __name__ == "__main__":
-    # Start the bot
     bot.start()
-    
-    # Print the bot's username
     uname = bot.get_me().username
     print(f"@{uname} Started Successfully. Made By @VJ_Botz 🤗")
-    
-    # Run the Flask app
-    app.run(host='0.0.0.0', port=PORT)
-
-    # Use idle to keep the bot running
-    idle()
-    
-    # Stop the bot when done
-    bot.stop()
-    print("Bot Stopped Bye!")
