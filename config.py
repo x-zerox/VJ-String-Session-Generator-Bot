a3e9b60de344293ddce4358bc404de7f28494661
x-zerox <ajprofessor0@gmail.com>
2025-01-31 19:05:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER_ID = int(environ.get("OWNER_ID", "5935267941")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
-F_SUB = environ.get("F_SUB", "-1001516086171")
+F_SUB = environ.get("F_SUB", "")
 
 # Mongodb Database Uri For User Data Store 
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
