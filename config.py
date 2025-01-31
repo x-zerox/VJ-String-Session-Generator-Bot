2a31372ac0421d15645e8b808d21c1391b356d51
x-zerox <ajprofessor0@gmail.com>
2025-01-31 16:57:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 from os import environ
 
 # Telegram Account Api Id And Api Hash
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "9328697"))
+API_HASH = environ.get("API_HASH", "9844b5bade2267c9a175a1dc72952e76")
 
 # Your Main Bot Token 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Owner ID For Broadcasting 
-OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
+OWNER_ID = int(environ.get("OWNER_ID", "5935267941")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
-F_SUB = environ.get("F_SUB", "")
+F_SUB = environ.get("F_SUB", "-1001516086171")
 
 # Mongodb Database Uri For User Data Store 
-MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://AJEPROF: AJEPROF@ajeprof.6evwa.mongodb.net/?retryWrites=true&w=majority&appName=AJEPROF")
 
 # Port To Run Web Application 
 PORT = int(environ.get('PORT', 8080))
